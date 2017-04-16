0640b1427acad0c47f1244b8f090737a667a1807
tweakimp <tweakimp@gmail.com>
2017-04-16 10:31:14 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
 # Twisted4
 
-<<<<<<< HEAD
 **Twisted4** is a Connect4 variant.  
 We are currently coding.
-=======
-Twisted4 is a Connect4 variant.  
-We are currently coding.  
-Please stand by.
->>>>>>> origin/master
--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 // jshint esversion:6
 // jshint browser: true
 // jshint devel: true
+
 // start with 7x7 matrix
-var matrix = Array(7)
+const matrix = Array(7)
 	.fill(null)
 	.map(() => Array(7)
 		.fill(0));
 
 // possible states a field can have. 0 for empty, 1 for player 1, 2 for player 2
-var fieldtoken = [0, 1, 2];
+const fieldtoken = [0, 1, 2];
 
 // switch to "coordinates", "randomized", "custom". coordinates makes the the fields show their coordinates; randomized creates a random 0,1,2 distribution; with custom you can setup a custom grid via customMatrix
-var entries = "custom";
+const entries = "randomized";
 
-// gravity = true makes zeroes get moved up in a column 
-var gravity = true;
+// gravity = true makes zeroes get moved up in a column
+const gravity = true;
 
 // we are currently just filling the board with a random distribution of the field tokens
 function randomEntry() {
@@ -24,7 +25,7 @@ function randomEntry() {
 // applies the custom matrix on the matrix the game uses
 function applyCustom() {
 	// the custom matrix is applied in a way so that the columns you see here are also the columns you will see in the browser
-	var customMatrix = [
+	const customMatrix = [
 		[0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0],
 		[1, 0, 0, 0, 0, 2, 0],
@@ -34,20 +35,20 @@ function applyCustom() {
 		[1, 2, 2, 1, 1, 1, 0],
 	];
 
-	for (var i = 6; i > -1; i--) {
-		for (var j = 6; j > -1; j--) {
+	for (let i = 6; i > -1; i--) {
+		for (let j = 6; j > -1; j--) {
 			matrix[i][j] = customMatrix[6 - j][i];
 		}
 	}
 }
 
-// fill the matrix with what you chose at var entries
+// fill the matrix with what you chose at let entries
 function fillMatrix() {
-	for (var i = 6; i > -1; i--) {
-		for (var j = 6; j > -1; j--) {
+	for (let i = 6; i > -1; i--) {
+		for (let j = 6; j > -1; j--) {
 			switch (entries) {
 				case "coordinates":
-					matrix[i][j] = "column" + i + "<br>row" + j;
+					matrix[i][j] = "column $(i) <br>row $(j)";
 					break;
 				case "randomized":
 					matrix[i][j] = randomEntry();
@@ -65,42 +66,42 @@ function fillMatrix() {
 fillMatrix();
 
 function applyGravity() {
-	for (var i = 0; i < 7; i++) {
+	for (let i = 0; i < 7; i++) {
 		// find zeroes and write their index to array
-		var zeroes = [];
+		let zeroes = [];
 
-		for (var j = 0; j < 7; j++) {
+		for (let j = 0; j < 7; j++) {
 			if (matrix[i][j] === 0) {
 				zeroes.push(j);
 			}
 		}
 		// splice zeroes from columns and push them to the end
-		for (var z = 0; z < zeroes.length; z++) {
-			//zeroes[z] needs -z so the focus stays on the right array item. saved indices need to be adjusted beucase we splice while reading them.
+		for (let z = 0; z < zeroes.length; z++) {
+			// zeroes[z] needs -z so the focus stays on the right array item. saved indices need to be adjusted beucase we splice while reading them.
 			matrix[i].splice(zeroes[z] - z, 1);
 			matrix[i].push(0);
 		}
 	}
 }
 
-// apply gravity if var gravity = true
+// apply gravity if let gravity = true
 if (gravity) {
 	applyGravity();
 }
 
 // convert javascript matrix into html matrix. note that we create columns from left to right and rows from bottom to top
 function drawMatrix() {
-	var gameArea = document.getElementsByClassName("gameArea")[0];
-	for (var i = 0; i < 7; i++) {
-		//create columns
-		var column = document.createElement("div");
+	const gameArea = document.getElementsByClassName("gameArea")[0];
+	for (let i = 0; i < 7; i++) {
+		// create columns
+		let column = document.createElement("div");
 		column.className = "column";
 		gameArea.appendChild(column);
-		for (var j = 6; j > -1; j--) {
-			//create fields			
-			var field = document.createElement("div");
+		for (let j = 6; j > -1; j--) {
+			// create fields
+			let field = document.createElement("div");
 			field.className = "field";
-			var textfield = document.createElement("div");
+			let textfield = document.createElement("div");
 			textfield.className = "textfield";
 			textfield.innerHTML = matrix[i][j];
 			column.appendChild(field);
@@ -111,23 +112,23 @@ function drawMatrix() {
 drawMatrix();
 
 // check for winconditions. devide them up into three to make it easier
-var winRow = false;
-var winColumn = false;
-var winDiagonal = false;
+let winRow = false;
+let winColumn = false;
+let winDiagonal = false;
 
 function checkRows() {
-	//four in a row
-	for (var j = 0; j < 7; j++) {
-		var currentCount = 1;
-		var currentToken = matrix[0][j];
-		for (var i = 1; i < 7; i++) {
+	// four in a row
+	for (let j = 0; j < 7; j++) {
+		let currentCount = 1;
+		let currentToken = matrix[0][j];
+		for (let i = 1; i < 7; i++) {
 			if (currentToken === matrix[i][j] && currentToken !== 0) {
 				currentCount++;
 				if (currentCount === 4) {
 					winRow = true;
-					for (var markSteps = 0; markSteps < currentCount; markSteps++) {
-						var markColumn = document.getElementsByClassName("column")[i - markSteps];
-						var markFields = markColumn.getElementsByClassName("field")[6 - j];
+					for (let markSteps = 0; markSteps < currentCount; markSteps++) {
+						let markColumn = document.getElementsByClassName("column")[i - markSteps];
+						let markFields = markColumn.getElementsByClassName("field")[6 - j];
 						markFields.className += " markedField";
 					}
 				}
@@ -140,18 +141,18 @@ function checkRows() {
 }
 
 function checkColumns() {
-	//four in a column
-	for (var i = 0; i < 7; i++) {
-		var currentCount = 1;
-		var currentToken = matrix[i][0];
-		for (var j = 1; j < 7; j++) {
+	// four in a column
+	for (let i = 0; i < 7; i++) {
+		let currentCount = 1;
+		let currentToken = matrix[i][0];
+		for (let j = 1; j < 7; j++) {
 			if (currentToken === matrix[i][j] && currentToken !== 0) {
 				currentCount++;
 				if (currentCount === 4) {
 					winColumn = true;
-					for (var markSteps = 0; markSteps < 4; markSteps++) {
-						var markColumn = document.getElementsByClassName("column")[i];
-						var markFields = markColumn.getElementsByClassName("field")[6 - (j - markSteps)];
+					for (let markSteps = 0; markSteps < 4; markSteps++) {
+						let markColumn = document.getElementsByClassName("column")[i];
+						let markFields = markColumn.getElementsByClassName("field")[6 - (j - markSteps)];
 						markFields.className += " markedField";
 					}
 					break;
@@ -165,11 +166,11 @@ function checkColumns() {
 }
 
 function checkBLTRdiagonals() {
-	for (var j = 0; j < 4; j++) {
-		for (var i = 0; i < 4; i++) {
-			var currentCount = 1;
-			var currentToken = matrix[i][j];
-			for (var step = 1; step < 4; step++) {
+	for (let j = 0; j < 4; j++) {
+		for (let i = 0; i < 4; i++) {
+			let currentCount = 1;
+			let currentToken = matrix[i][j];
+			for (let step = 1; step < 4; step++) {
 				if (currentToken === matrix[i + step][j + step] && currentToken !== 0) {
 					currentCount++;
 				} else {
@@ -177,11 +178,11 @@ function checkBLTRdiagonals() {
 				}
 				if (currentCount === 4) {
 					winDiagonal = true;
-					//mark winning fields
-					//document.getElementsByClassName("column")[i].getElementsByClassName("field")[6 - j].className += " markedField";
-					for (var markSteps = 0; markSteps < 4; markSteps++) {
-						var markColumn = document.getElementsByClassName("column")[i + markSteps];
-						var markFields = markColumn.getElementsByClassName("field")[6 - (j + markSteps)];
+					// mark winning fields
+					// document.getElementsByClassName("column")[i].getElementsByClassName("field")[6 - j].className += " markedField";
+					for (let markSteps = 0; markSteps < 4; markSteps++) {
+						let markColumn = document.getElementsByClassName("column")[i + markSteps];
+						let markFields = markColumn.getElementsByClassName("field")[6 - (j + markSteps)];
 						markFields.className += " markedField";
 					}
 					break;
@@ -192,11 +193,11 @@ function checkBLTRdiagonals() {
 }
 
 function checkTLBRdiagonals() {
-	for (var i = 0; i < 4; i++) {
-		for (var j = 6; j > 3; j--) {
-			var currentCount = 1;
-			var currentToken = matrix[i][j];
-			for (var step = 1; step < 4; step++) {
+	for (let i = 0; i < 4; i++) {
+		for (let j = 6; j > 3; j--) {
+			let currentCount = 1;
+			let currentToken = matrix[i][j];
+			for (let step = 1; step < 4; step++) {
 				if (currentToken === matrix[i + step][j - step] && currentToken !== 0) {
 					currentCount++;
 				} else {
@@ -204,11 +205,11 @@ function checkTLBRdiagonals() {
 				}
 				if (currentCount === 4) {
 					winDiagonal = true;
-					//mark winning fields
-					//document.getElementsByClassName("column")[i].getElementsByClassName("field")[6 - j].className += " markedField";
-					for (var markSteps = 0; markSteps < 4; markSteps++) {
-						var markColumn = document.getElementsByClassName("column")[i + markSteps];
-						var markFields = markColumn.getElementsByClassName("field")[6 - (j - markSteps)];
+					// mark winning fields
+					// document.getElementsByClassName("column")[i].getElementsByClassName("field")[6 - j].className += " markedField";
+					for (let markSteps = 0; markSteps < 4; markSteps++) {
+						let markColumn = document.getElementsByClassName("column")[i + markSteps];
+						let markFields = markColumn.getElementsByClassName("field")[6 - (j - markSteps)];
 						markFields.className += " markedField";
 					}
 					break;
@@ -219,12 +220,12 @@ function checkTLBRdiagonals() {
 }
 
 function checkDiagonals() {
-	//four in a diagonal
-	//bottom left to top right diagonals
-	//idea: search from start points for these diagonals. start points are the fields in the bottom left 4*4 square
+	// four in a diagonal
+	// bottom left to top right diagonals
+	// idea: search from start points for these diagonals. start points are the fields in the bottom left 4*4 square
 	checkBLTRdiagonals();
-	//top left to bottom right diagonals
-	//idea: search from start points for these diagonals. start points are the fields in the top left 4*4 square
+	// top left to bottom right diagonals
+	// idea: search from start points for these diagonals. start points are the fields in the top left 4*4 square
 	checkTLBRdiagonals();
 }
 
