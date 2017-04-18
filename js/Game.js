eb81c5d71cc4336c700039068794953a8ba6f687
mariansauter <marsaute@gmail.com>
2017-04-18 15:06:23 +0200

    Added basic AI
    AI to be found in "Player"
    
    For test purposes, removed left/right twists form legalmoves
    
    Minor fixes

--- js/Game.js
+++ js/Game.js
@@ -4,84 +4,88 @@
 
 function Game(player1, player2, timelimit) {
 
-	this.timelimit = timelimit; // time per move
+    this.timelimit = timelimit; // time per move
 
-	this.player1 = new Player("Bert", "ai");
-	this.player2 = new Player("Ernie", "ai");
+    this.player1 = new Player(1, "random");
+    this.player2 = new Player(2, "ai");
 
-	this.randomturn = function () {
-		return Math.floor(Math.random() * 2) + 1;
-	};
-	this.turnnumber = 1; // turnnumber > 49 -> draw
-	this.board = new GameArea("start", "true");
+    this.randomturn = function () {
+        let legal = twisted.getLegalMoves();
+        let move = legal[Math.floor(Math.random() * legal.length)];
+        return move; // return item from legalmoves
+    };
+    this.turnnumber = 1; // turnnumber > 49 -> draw
+    this.board = new GameArea("start", true);
 
-	this.legalmoves = ["l", "r", 0, 1, 2, 3, 4, 5, 6]; // 1-7 spalten,  0 links drehen, 8 rechts drehen
+    //this.legalmoves = ["l", "r", 0, 1, 2, 3, 4, 5, 6]; // 1-7 spalten
+    this.legalmoves = [0, 1, 2, 3, 4, 5, 6]; // 1-7 spalten
 
-	this.eventListener = 0; // gucken wer wo hin klickt mouse over spalte, bei click zug in der spalte
-	this.time = 0;
-	this.countdown = function () {
-		this.time--;
-		// time limit reached, lose game
-		if (this.time === 0) {
-			if (this.whoseturn === 1) {} else {}
-		}
+    this.time = 0;
+    this.countdown = function () {
+        this.time--;
+        // time limit reached, lose game
+        if (this.time === 0) {
+            if (this.whoseturn === 1) {} else {}
+        }
 
-	};
-	this.history = [];
+    };
+    this.history = [];
 
-	this.getLegalMoves = function () {
-		return this.legalmoves;
-	};
+    this.getLegalMoves = function () {
+        return this.legalmoves;
+    };
 
-	this.start = function () {
+    this.start = function () {
+        var customMatrix = [
+			[0, 0, 0, 0, 0, 0, 2],
+			[0, 0, 0, 0, 0, 0, 2],
+			[1, 0, 0, 0, 0, 0, 2],
+			[1, 0, 0, 0, 0, 0, 2],
+			[1, 0, 0, 0, 0, 0, 2],
+			[1, 0, 0, 0, 0, 0, 1],
+			[1, 0, 2, 2, 2, 2, 2],
+		];
+        this.nextTurn();
+    };
 
-		this.nextTurn();
-	};
+    this.nextTurn = function () {
+        /*
+        this.time = timelimit;
+                var timer = setInterval(function () {
+                    this.time--;
+                    // time limit reached, lose game
+                    if (this.time === 0) {
+                        if (this.whoseturn === 1) {} else {}
+                    }
+                }, 1000);
+        */
+        let move;
+        let player;
+        if (this.turnnumber % 2 === 1) {
+            move = this.player1.getMove();
+            player = 1;
+        } else {
+            move = this.player2.getMove();
+            player = 2;
+        }
 
-	this.nextTurn = function () {
-		/*
-		this.time = timelimit;
-		        var timer = setInterval(function () {
-		            this.time--;
-		            // time limit reached, lose game
-		            if (this.time === 0) {
-		                if (this.whoseturn === 1) {} else {}
-		            }
-		        }, 1000);
-		*/
-		let move;
-		let player;
-		if (this.turnnumber % 2 === 1) {
-			move = this.player1.getMove();
-			player = 1;
-		} else {
-			move = this.player2.getMove();
-			player = 2
-		}
+        this.history.push(move);
+        this.board.makeMove(move, player);
 
-		this.history.push(move);
-		this.board.makeMove(move, player);
+        // check HERE for wins
 
-		// check HERE for wins
+        this.board.deleteMatrix();
+        this.board.drawMatrix();
 
-		this.board.deleteMatrix();
-		this.board.drawMatrix();
+        // clearInterval(timer);
 
-		// clearInterval(timer);
+        this.turnnumber++;
+        if (this.turnnumber === 12) {
+            throw "mach ma schluss";
+        }
 
-		this.turnnumber++;
-		if (this.turnnumber === 20) {
-			throw "mach ma schluss";
-		}
-		sleep(500)
-			.then(() => {
-				this.nextTurn();
-			});
-
-		// setTimeout(this.nextTurn(), 1000);
-	}
-};
-// sleep time expects milliseconds
-function sleep(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
+        setTimeout(function () {
+            twisted.nextTurn();
+        }, 500);
+    };
 }
